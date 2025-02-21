f7c0d47ebb41b8ecac262d1f7f9cc9b61203599e
Damiansjj <ellis.viracocha.getial@student.ehb.be>
2025-02-21 14:56:16 +0100

    Update M1.js

--- Module 1/M1.js
+++ Module 1/M1.js
@@ -5,9 +5,7 @@ specialElements.forEach(element => {
     element.style.color = 'red';
 });
 
-
-const tweedeParagraaf = document.getElementById('tweede');
-tweedeParagraaf.classList.add('onderstreept');
-
+const tweedeParagraaf = document.querySelectorAll('p')[1]; 
+tweedeParagraaf.style.textDecoration = 'underline'; 
 
 document.getElementById('output').innerText = 'Aantal special elementen: ' + specialElements.length;
