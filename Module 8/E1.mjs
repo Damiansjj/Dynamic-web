876f46d9d7a7bebe4d2fe639a1ccdd9d5e65eb8b
Damiansjj <ellis.viracocha.getial@student.ehb.be>
2025-04-01 01:36:19 +0200

    Update E1.mjs

--- Module 8/E1.mjs
+++ Module 8/E1.mjs
@@ -1,10 +1,8 @@
-// Functie om het thema in te stellen
 const setTheme = (theme) => {
     document.body.className = theme;
     localStorage.setItem('theme', theme);
 };
 
-// Functie om het thema te schakelen
 const toggleTheme = () => {
     const currentTheme = localStorage.getItem('theme');
     if (currentTheme === 'dark-theme') {
@@ -16,16 +14,13 @@ const toggleTheme = () => {
     }
 };
 
-// Laad het opgeslagen thema bij het laden van de pagina
 document.addEventListener('DOMContentLoaded', () => {
     const savedTheme = localStorage.getItem('theme') || 'light-theme';
     setTheme(savedTheme);
 
-    // Update de knoptekst op basis van het huidige thema
     if (savedTheme === 'dark-theme') {
         document.getElementById('themeToggle').textContent = 'Schakel naar Licht Thema';
     }
 
-    // Voeg eventlistener toe aan de knop
     document.getElementById('themeToggle').addEventListener('click', toggleTheme);
 });
